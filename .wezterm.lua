e9f6e50be2262ef805685a3e68021ce1adadf185
zackaj <aktouf98zakaria@gmail.com>
2024-08-09 19:01:59 +0100

    cascadia code PL for bigger icos

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,7 @@ config.window_background_opacity = 0.7
 
 -- config.font = wezterm.font("JetBrains Mono Nerd Font Mono", { weight = "Bold", italic = true })
 
-config.font = wezterm.font("Cascadia Code NF", { weight = "Bold", italic = true })
+config.font = wezterm.font("Cascadia Code PL", { weight = "Bold", italic = true })
 config.font_size = 13
 
 config.window_decorations = "RESIZE"
