1b2f34c3228a5440beeac1879b405bc993e59be7
zackaj <aktouf98zakaria@gmail.com>
2024-08-24 22:45:37 +0100

    added rtl support ( it still bad an possibly a deal breaker )

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,6 +7,7 @@ config.window_background_opacity = 0.7
 config.font = wezterm.font("Cascadia Code PL", { weight = "Bold", italic = true })
 config.font_size = 13
 
+config.bidi_enabled = true
 config.window_decorations = "RESIZE"
 
 config.colors = {
